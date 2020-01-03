bd1c7258c28947b8f18d9557282759cb2744e69b
John Joseph <atiquajohn@gmail.com>
2020-01-03 17:47:48 +0700

    write test for new game module

---
+++ test/game_test.exs
@@ -0,0 +1,13 @@
+defmodule GameTest do
+  use ExUnit.Case
+  doctest Hangman
+
+  alias Hangman.Game
+
+  test "new_game return structure" do
+    game = Game.new_game()
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+end
