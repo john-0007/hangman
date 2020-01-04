200d41fe3a3746cbf10e7cef1772cd2c2f5b43a1
John Joseph <atiquajohn@gmail.com>
2020-01-04 23:39:54 +0700

    added new modules and test

--- test/game_test.exs
+++ test/game_test.exs
@@ -10,4 +10,28 @@ defmodule GameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game =
+        Game.new_game()
+        |> Map.put(:game_state, state)
+
+      assert {^game, _} = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not aready used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
 end
